691cf241d12f3ad7acddd9132498a3e4eb93196b
DavidDev2002 <dvazquez@institutmvm.cat>
2021-12-02 16:01:23 +0100

    Nuevo ejercicio

--- main.py
+++ main.py
@@ -5,10 +5,11 @@
 
 
 def main():
+    #Ejercicio de Codigo de nombre
     nombre = input("Introduce tu nombre: ")
     apellido = input("Introduce tu apellido: ")
     segundo = input("Introduce tu 2º apellido: ")
 
-    print("Este es tu codigo: ",apellido[:2],segundo[:2],nombre[:2])
+    print("Este es tu codigo: ",apellido[:2] + segundo[:2] + nombre[:2])
 if __name__ == '__main__':
     main()
